69988e73c0951092eea42d675dbe7a1be8aa4637
Gaetan SENN <gaetan.senn@gmail.com>
2020-11-30 14:59:32 +0100

    feat: add DwContainer

---
+++ lib/components/basic/Container.vue
@@ -0,0 +1,14 @@
+<template>
+  <div :class="[config.fixed]">
+    <slot />
+  </div>
+</template>
+
+<script>
+import config from '../config.mixin'
+
+export default {
+  configPath: 'Container',
+  mixins: [config]
+}
+</script>
