3a4bd47d896e248b7cd097af8c72e3fe1d1940c7
Gaetan SENN <gaetan.senn@gmail.com>
2020-11-30 19:27:23 +0100

    feat(collapse): add storybook stories

--- lib/components/others/collapse/CollapseItem.vue
+++ lib/components/others/collapse/CollapseItem.vue
@@ -70,7 +70,7 @@ export default {
   },
   computed: {
     isActive () {
-      return this.collapse.activeNames.includes(this.name)
+      return this.collapse && this.collapse.activeNames.includes(this.name)
     }
   },
   methods: {
