b1752ae9461c6875608727303b5e5d1f8918e41f
Gaetan SENN <gaetan.senn@gmail.com>
2020-12-14 14:23:27 +0100

    feat: add header footer and collapse to DwCard

--- lib/components/others/Card.vue
+++ lib/components/others/Card.vue
@@ -12,6 +12,11 @@ export default {
     ...HrefProps,
     ...ToProps
   },
+  data () {
+    return {
+      opened: false
+    }
+  },
   computed: {
     is () {
       if (this.href)
@@ -57,10 +62,52 @@ export default {
     }
   },
   render (h) {
+    const childrens = []
+    const content = {
+      class: [this.config.content.fixed, this.config.content.classes]
+    }
+
+    if (this.header || this.$slots.header) {
+      const headerChildrens = []
+
+      if (this.$slots.header) headerChildrens.push(this.$slots.header)
+      else headerChildrens.push(this.header)
+      if (this.collapse) headerChildrens.push(h('div', {
+        class: [this.config.header.icon.fixed, this.config.header.icon.classes],
+        domProps: { innerHTML: this.config.header.icon.icon }
+      }))
+
+      childrens.push(h('div', {
+        class: [this.config.header.fixed, this.config.header.classes],
+        on: {
+          click: () => {
+            this.opened = !this.opened
+          }
+        }
+      }, headerChildrens))
+    }
+
+    if (this.collapse)
+      childrens.push(h('DwCollapseTransition', [h('div', {
+        directives: [{
+          name: 'show',
+          rawName: 'v-show',
+          value: (this.opened),
+          expression: 'opened'
+        }],
+        ...content
+      }, [this.$slots.default])]))
+    else
+      childrens.push(h('div', content, [this.$slots.default]))
+
+    if (this.$slots.footer) childrens.push(h('div', {
+      class: [this.config.footer.fixed, this.config.footer.classes]
+    }, [this.$slots.footer]))
+
     return h(this.is, {
       ...this.bind,
       class: this.config.fixed
-    }, this.$slots.default)
+    }, childrens)
   }
 }
 </script>
