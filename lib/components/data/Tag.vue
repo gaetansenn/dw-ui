38dfda9e57329f99f43bffb5b6c8c4db9140f1fe
Gaetan SENN <gaetan.senn@gmail.com>
2021-01-07 15:22:58 +0100

    feat: add DwTag

---
+++ lib/components/data/Tag.vue
@@ -0,0 +1,22 @@
+<template>
+  <span :class="[config.fixed, config.rounded, config.variant, config.size]">
+    <slot />
+  </span>
+</template>
+
+<script>
+import config from '../config.mixin'
+import CommonsProps from '../commons.props'
+import RoundedProps from '../rounded.props'
+import SizeProps from '../size.props'
+
+export default {
+  configPath: 'Tag',
+  mixins: [config],
+  props: {
+    ...RoundedProps,
+    ...CommonsProps,
+    ...SizeProps
+  }
+}
+</script>
