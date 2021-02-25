8f508e23db2e32585a9162af6c7e2d6c8a3a6f44
Gaetan SENN <gaetan.senn@gmail.com>
2021-02-25 14:54:03 +0100

    fix: fix scroll

--- lib/components/others/ScrollWrapper.vue
+++ lib/components/others/ScrollWrapper.vue
@@ -1,14 +1,14 @@
 <template>
   <div :class="config.wrapper">
-    <slot v-if="index" name="prev" :prev="prev">
+    <slot v-if="scrollPosition > scrollStart" name="prev" :prev="prev">
       <div :class="config.navigation.prev.wrapper" @click="prev">
         <div :class="config.navigation.prev.fixed" v-html="config.navigation.prev.icon" />
       </div>
     </slot>
-    <div ref="content" :class="config.fixed">
+    <div ref="content" :class="config.fixed" @scroll="onScroll">
       <slot name="default" />
     </div>
-    <slot v-if="index < positions.length - 1" name="next" :next="next">
+    <slot v-if="scrollPosition !== scrollMax" name="next" :next="next">
       <div :class="config.navigation.next.wrapper" @click="next">
         <div :class="config.navigation.next.fixed" v-html="config.navigation.next.icon" />
       </div>
@@ -31,14 +31,21 @@ export default {
   data () {
     return {
       index: this.activeIndex,
-      positions: []
+      positions: [],
+      scrollable: false,
+      scrollPosition: 0,
+      scrollMax: 0,
+      parentWidth: 0
     }
   },
   mounted () {
     // Disable if not scrollable
-    if (this.$refs.content.offsetWidth === this.$refs.content.scrollWidth) return
+    this.scrollable = (this.$refs.content.offsetWidth !== this.$refs.content.scrollWidth)
 
-    const parentWidth = this.$el.offsetWidth
+    if (!this.scrollable) return
+
+    this.scrollMax = this.$refs.content.scrollWidth - this.$refs.content.clientWidth
+    this.parentWidth = this.$refs.content.offsetWidth
     this.positions = this.$slots.default.map(children => ({
       offset: children.elm.offsetLeft,
       width: children.elm.offsetWidth
@@ -49,41 +56,41 @@ export default {
       })
 
       return accu
-    }, []).filter(item => item.to <= parentWidth)
+    }, []).filter(item => item.to <= this.parentWidth)
+
+    this.scrollStart = this.positions[0].from
 
     // Scroll to index
     if (this.index) this.$el.scrollTo(this.index)
-
-    // Update current index according to scroll x position
-    this.$refs.content.addEventListener('scroll', (event) => {
+  },
+  methods: {
+    onScroll (event) {
+      this.scrollPosition = event.target.scrollLeft
       // Find index according to scroll value
       const index = this.positions.findIndex((value) => {
         return value.from <= event.target.scrollLeft && event.target.scrollLeft <= value.to
       })
 
-      this.index = index !== -1 ? index : this.positions.length - 1
-    })
-  },
-  methods: {
+      if (index === -1 && event.target.scrollLeft < this.positions[0].from) this.index = 0
+      else this.index = index !== -1 ? index : this.positions.length - 1
+    },
     scrollToIndex (index) {
-      if (this.positions.length - 1 < index) return
+      if (!this.scrollable) return true
 
-      this.$refs.content.scrollTo({
-        left: index > this.positions.length - 1 ? this.positions[this.positions.length - 1].to + 10000 : this.positions[index].from,
-        behavior: 'smooth'
+      this.$nextTick(() => {
+        this.$refs.content.scrollTo({
+          left: index > this.positions.length - 1 ? this.positions[this.positions.length - 1].to + 10000 : this.positions[index].from,
+          behavior: 'smooth'
+        })
       })
+
+      return true
     },
     prev () {
-      if (this.index !== 0) {
-        this.index--
-        this.scrollToIndex(this.index)
-      }
+      this.scrollToIndex(this.index - 1)
     },
     next () {
-      if (this.index + 1 < this.positions.length) {
-        this.index++
-        this.scrollToIndex(this.index)
-      }
+      this.scrollToIndex(this.index + 1)
     }
   }
 }
