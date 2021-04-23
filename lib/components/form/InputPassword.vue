cb30c6a00a6cafe29caaeff0081fd4d1e42df02a
Gaetan SENN <gaetan.senn@gmail.com>
2021-04-23 12:54:05 +0200

    feat: change password regex

--- lib/components/form/InputPassword.vue
+++ lib/components/form/InputPassword.vue
@@ -29,7 +29,7 @@ import FormProps from './Form.props'
 
 export function validate () {
   // eslint-disable-next-line
-  const regex = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[ !"#$%&'()*+,-.\/:;<=>?@[\]^_`{|}~])[A-Za-z\d !"#$%&'()*+,-.\/:;<=>?@[\]^_`{|}~]{8,}$/
+  const regex = /^(?=.{8,})(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+*!='(),-.\/:;<>?[\]_`{|}~]).*$/
 
   const test = regex.test(this.value)
 
