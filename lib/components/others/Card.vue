a142e45ee733b2a7f0baf2b12c44e42f61088ef5
Gaetan SENN <gaetan.senn@gmail.com>
2021-04-26 10:21:37 +0200

    feat: add toggle event for card

--- lib/components/others/Card.vue
+++ lib/components/others/Card.vue
@@ -87,6 +87,7 @@ export default {
 
       if (this.collapse) header.on = {
         click: () => {
+          this.$emit('toggle', !this.opened)
           this.opened = !this.opened
         }
       }
