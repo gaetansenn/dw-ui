1201f14550be777295200762a9304e263824bc30
Gaetan SENN <gaetan.senn@gmail.com>
2021-12-13 17:06:48 +0100

    feat: handle custom InputGroup in InputGroupProps

--- lib/components/form/InputGroupCheckbox.vue
+++ lib/components/form/InputGroupCheckbox.vue
@@ -26,7 +26,8 @@ export default {
     inputGroupProps () {
       return {
         ...syncProps.call(this, Object.keys({ ...CommonsProps, ...SizeProps, ...InputGroupCheckboxProps, ...InputGroupProps })),
-        validation: this.localValidation
+        validation: this.localValidation,
+        rootPath: 'InputGroupCheckbox'
       }
     }
   },
